1aa5e2ac23eb8815fe8cd0b7387428f4325945bb
Yakov <bluehome88@outlook.com>
2018-07-23 11:51:57 +0300

    active admin auth option

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,10 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     end
   	def after_sign_in_path_for(resource)
-      "/tree"
+      if resource.is_a?(AdminUser)
+        admin_dashboard_path
+      else
+        "/tree"
+      end
 	end  
 end
